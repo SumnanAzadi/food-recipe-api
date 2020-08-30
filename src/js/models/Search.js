99d2e4cd5bf3fa3a1248de633509b69887dbd788
SumnanAzadi <anadaplay@gmail.com>
2020-08-30 23:06:53 +0600

    Initial search setup

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,26 @@
-export default "I am an exported string";
+// Data for the search is just query and search results.
+
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults() {
+    /* //When we use proxy with key. In here we don't need these.
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const key = "462b1cc8d4f2730081462fbc65136320"; */
+    try {
+      /* const res = await axios(
+        `${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`
+      ); */
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
